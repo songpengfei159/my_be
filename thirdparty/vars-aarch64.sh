314e6a79037ac90b92bf3acd8f27006fdc37a1df
songpengfei159 <62453456+songpengfei159@users.noreply.github.com>
2025-11-09 16:30:13 +0800

    feat: add third part

---
+++ thirdparty/vars-aarch64.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright 2021-present StarRocks, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#####################################################
+# Download url, filename and unpaced filename
+# of all thirdparties
+# 
+# vars-${arch}.sh defines the thirdparties that are
+# architecure-related.
+#####################################################
+
+# HADOOP
+#HADOOP_DOWNLOAD="https://archive.apache.org/dist/hadoop/common/hadoop-3.4.0/hadoop-3.4.0-aarch64.tar.gz"
+#HADOOP_NAME="hadoop-3.4.0-aarch64.tar.gz"
+#HADOOP_SOURCE="hadoop-3.4.0"
+#HADOOP_MD5SUM="4cf40e127f27044310aae36dce23bdb1"
+
+# OPEN JDK FOR aarch64, provided by huawei kunpeng (https://www.hikunpeng.com/zh/developer/devkit/compiler)
+JDK_DOWNLOAD="https://mirror.iscas.ac.cn/kunpeng/archive/compiler/bisheng_jdk/bisheng-jdk-8u262-linux-aarch64.tar.gz"
+JDK_NAME="bisheng-jdk-8u262-linux-aarch64.tar.gz"
+JDK_SOURCE="bisheng-jdk1.8.0_262"
+JDK_MD5SUM="a1254dea3728e0a86e53a55d8debfbeb"
+
+# HYPERSCAN for aarch64, provided by huawei kunpeng.
+HYPERSCAN_DOWNLOAD="https://github.com/kunpengcompute/hyperscan/archive/refs/tags/v5.3.0.aarch64.tar.gz"
+HYPERSCAN_NAME="hyperscan-5.3.0.aarch64.tar.gz"
+HYPERSCAN_SOURCE="hyperscan-5.3.0.aarch64"
+HYPERSCAN_MD5SUM="ef337257bde6583242a739fab6fb161f"
+
+# async-profiler
+ASYNC_PROFILER_DOWNLOAD="https://github.com/async-profiler/async-profiler/releases/download/v2.9/async-profiler-2.9-linux-arm64.tar.gz"
+ASYNC_PROFILER_NAME="async-profiler-2.9-linux-arm64.tar.gz"
+ASYNC_PROFILER_SOURCE="async-profiler-2.9-linux-arm64"
+ASYNC_PROFILER_MD5SUM="d31a70d2c176146a46dffc15948040ed"
+
+# jindosdk for Aliyun OSS
+JINDOSDK_DOWNLOAD="https://cdn-thirdparty.starrocks.com/jindosdk-4.6.8-linux-el7-aarch64.tar.gz"
+JINDOSDK_NAME="jindosdk-4.6.8-linux-el7-aarch64.tar.gz"
+JINDOSDK_SOURCE="jindosdk-4.6.8-linux-el7-aarch64"
+JINDOSDK_MD5SUM="27a4e2cd9a403c6e21079a866287d88b"
+
+# starcache
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.3.3/starcache-centos7_arm64.tar.gz"
+STARCACHE_NAME="starcache.tar.gz"
+STARCACHE_SOURCE="starcache"
+STARCACHE_MD5SUM="545ca196d5591c9e6925788ba363c313"
---
+++ thirdparty/vars-x86_64.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright 2021-present StarRocks, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#####################################################
+# Download url, filename and unpaced filename
+# of all thirdparties
+# 
+# vars-${arch}.sh defines the thirdparties that are
+# architecure-related.
+#####################################################
+
+# HADOOP
+HADOOP_DOWNLOAD="https://archive.apache.org/dist/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz"
+HADOOP_NAME="hadoop-3.4.0.tar.gz"
+HADOOP_SOURCE="hadoop-3.4.0"
+HADOOP_MD5SUM="2f9244ab73169cf7bc0811d932067f6b"
+
+# OPEN JDK
+JDK_DOWNLOAD="https://cdn-thirdparty.starrocks.com/java-se-8u41-ri.tar.gz"
+JDK_NAME="java-se-8u41-ri.tar.gz"
+JDK_SOURCE="java-se-8u41-ri"
+JDK_MD5SUM="7295b5a3fb90e7aaf80df23d5eac222d"
+
+# HYPERSCAN
+HYPERSCAN_DOWNLOAD="https://github.com/intel/hyperscan/archive/v5.4.0.tar.gz"
+HYPERSCAN_NAME="hyperscan-5.4.0.tar.gz"
+HYPERSCAN_SOURCE="hyperscan-5.4.0"
+HYPERSCAN_MD5SUM="65e08385038c24470a248f6ff2fa379b"
+
+# async-profiler
+ASYNC_PROFILER_DOWNLOAD="https://github.com/async-profiler/async-profiler/releases/download/v2.9/async-profiler-2.9-linux-x64.tar.gz"
+ASYNC_PROFILER_NAME="async-profiler-2.9-linux-x64.tar.gz"
+ASYNC_PROFILER_SOURCE="async-profiler-2.9-linux-x64"
+ASYNC_PROFILER_MD5SUM="29127cee36b7acf069d31603b4558361"
+
+# jindosdk for Aliyun OSS
+JINDOSDK_DOWNLOAD="https://cdn-thirdparty.starrocks.com/jindosdk-4.6.8-linux.tar.gz"
+JINDOSDK_NAME="jindosdk-4.6.8-linux.tar.gz"
+JINDOSDK_SOURCE="jindosdk-4.6.8-linux"
+JINDOSDK_MD5SUM="5436e4fe39c4dfdc942e41821f1dd8a9"
+
+# starcache
+STARCACHE_DOWNLOAD="https://cdn-thirdparty.starrocks.com/starcache/v3.3.3/starcache-centos7_amd64.tar.gz"
+STARCACHE_NAME="starcache.tar.gz"
+STARCACHE_SOURCE="starcache"
+STARCACHE_MD5SUM="595d1b5d0c7935e1d1f91bf43059e461"
